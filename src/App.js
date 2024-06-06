dbc886abab6f083513719ae8efc5723f40a95c4b
Yagnesh Bharadava <157809480+yagness@users.noreply.github.com>
2024-06-06 20:26:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
+
+import axios from 'axios';
 import './App.css';
+import Category from './Category';
+import { useEffect, useState } from 'react';
 
 function App() {
+  let [finalCategory,setFinalcategory]=useState([])
+  let [finalProduct,setFinalProduct]=useState([])
+  let [catName,setCatname]=useState("")
+
+  let getCategory=()=>{
+    axios.get('http://dummyjson.com/products/categories')
+    .then((res)=>res.data)
+   // .then((ree)=>ree['name'])
+    .then((finalres)=>{
+      setFinalcategory(finalres)
+    })
+  }
+
+  useEffect(()=>{
+    getCategory();
+    getProduct()
+  },[])
+
+  useEffect(()=>{
+    if(catName!==""){
+      axios.get(`https://dummyjson.com/products/category/${catName}`)
+      .then((pro)=>pro.data)
+      .then((finalpro)=>{
+        setFinalProduct(finalpro.products)
+      })
+    }
+   
+  },[catName])
+
+  let getProduct=()=>{
+    axios.get('https://dummyjson.com/products')
+    .then((pro)=>pro.data)
+    .then((finalpro)=>{
+        setFinalProduct(finalpro.products)
+      //  console.log(finalpro)
+    })
+  }
+
+  let Pitem= finalProduct.map((products,index)=>{
+    return (
+      <ProductItem key={index} pdata={products} />
+    )
+
+  })
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='py-[40px]'>
+      <div className='max-w-[1320px] mx-auto'>
+        <h1 className='text-center text-[40px] font-bold mb-[30px]'>Our Products</h1>
+        <div className='grid grid-cols-[30%_auto] gap-[20px]'>
+          <div>
+            <Category finalCategory={finalCategory} setCatname={setCatname}/>
+          </div>
+          <div>
+            <div className='grid grid-cols-3 gap-5'>
+              { finalProduct.length>=1 
+              ?
+              Pitem
+              :
+              'No products found'
+              }
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+       
+    </>
   );
 }
 
 export default App;
+
+function ProductItem({pdata}){
+ // console.log(pdata)
+  return(
+    <div className='shadow-ig text-center pb-4 bg-[#ffffff]'>
+                <img src={pdata.thumbnail} className='w-[100%] h-[220px]'></img>
+                <h4>{pdata.title}</h4>
+                <b>Rs {pdata.price}</b>
+              </div>
+    
+  )
+}
---
+++ src/Category.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Category({finalCategory,setCatname}) {
+    let cat= finalCategory.map((v,i)=>{
+        return(
+            <li onClick={()=>setCatname(v.name)} key={i} className='bg-[#ccc] p-[7px] cursor-pointer text-[20px] font-serif font-[500] mb-2'>{v.name}</li>
+
+        )
+    })
+
+
+  return (
+    <div>
+        <h3 className='text-[25px] font-[500] p-[10px]'>Product Category</h3>
+        <ul>
+           {cat}
+        </ul>
+    </div>
+  )
+}
